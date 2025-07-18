b8c325c7108d2dbc96a0463c95d2dc891d30cdea
Hemanth Doddapaneni <127778946+Hemanth8448@users.noreply.github.com>
2025-07-18 22:14:57 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,10 +24,11 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+    
+      console.log('connected to db & listening on port')
+  })
   })
   .catch((error) => {
     console.log(error)
   })
+export default app
